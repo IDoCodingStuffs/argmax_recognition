e78be535e1b9424dc450b0d428117241a0b4869f
the-layman <sahinvictor@gmail.com>
2024-07-11 02:04:04 -0700

    Inference server bits

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 ## Execution
 - Run `gradlew bootRun` at the repository root to launch the request serving layer service
-- 
+- Run `src\main\python\inference_server.py
--- src/main/python/inference_server.py
+++ src/main/python/inference_server.py
@@ -1,12 +1,28 @@
 from flask import Flask, request
+from transformers import AutoImageProcessor, ResNetForImageClassification
+import torch
+import json
+
+# !TODO: Local caching
+processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
 
 app = Flask(__name__)
 
 @app.route('/run_inference', methods=["POST"])
 def run_inference():
-    body = request.form
-    print(body)
-    return ""
+    image_blob = request.form
+    if image_blob:
+        # !TODO: The actual transformation to a PIL image
+        inputs = processor(image_blob, return_tensors="pt")
+        with torch.no_grad():
+            logits = model(**inputs).logits
 
+        predicted_label = logits.argmax(-1).item()
+
+        return json.dumps({"predicted_label": predicted_label})
+    else:
+        return json.dumps({"predicted_label": ""})
+    
 if __name__ == '__main__':
     app.run()
